b18590166044fd8233fe63c4934ed66ecefff95d
taylornok <taylorowenskees@outlook.com>
2018-08-18 18:06:24 -0700

    home

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+// Dependencies
+// ===========================================================
+const express = require("express");
+const routes
+
+var app = express();
+var PORT = 3000;
+
+// Data
+// ===========================================================
