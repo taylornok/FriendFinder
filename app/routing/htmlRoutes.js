159cc576d5649df5686fb5bc389ca7b6f810cda5
taylornok <taylorowenskees@outlook.com>
2018-08-20 13:37:48 -0700

    update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,15 @@
-// Dependencies
-// ===========================================================
-const express = require("express");
-const routes
+//----------------------Dependencies--------------------------------
+var path = require('path');
 
-var app = express();
-var PORT = 3000;
 
-// Data
-// ===========================================================
+module.exports = function(app){
+  //default GET route that leads to home.html - displays home page
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
+
+  //a USE route to home page
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+};
